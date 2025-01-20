3b88b9b183461abf34ff8e1cf56717c177a2b5ca
19Phin <19phin@gmail.com>
2025-01-19 17:30:52 -0700

    1.20.5 - colorfulhearts crashes, not my problem

--- neoforge/src/main/java/net/dialingspoon/partialhearts/neoforge/PartialHeartsNeoForge.java
+++ neoforge/src/main/java/net/dialingspoon/partialhearts/neoforge/PartialHeartsNeoForge.java
@@ -5,11 +5,14 @@ import net.dialingspoon.partialhearts.gui.PatternListScreen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@Mod(value = PartialHearts.MOD_ID, dist = Dist.CLIENT)
+@Mod(PartialHearts.MOD_ID)
 public final class PartialHeartsNeoForge {
     public PartialHeartsNeoForge(ModContainer modContainer) {
-        modContainer.registerExtensionPoint(IConfigScreenFactory.class, (container, screen) -> new PatternListScreen(screen));
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            modContainer.registerExtensionPoint(IConfigScreenFactory.class, (container, screen) -> new PatternListScreen(screen));
+        }
     }
 }
