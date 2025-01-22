6455ed8f0b51eb6dec76b39fc647012f2e767d0f
19Phin <19phin@gmail.com>
2025-01-21 23:04:10 -0700

    backprop and bugfix

--- fabric/src/main/java/net/dialingspoon/partialhearts/fabric/mixin/GuiMixin.java
+++ fabric/src/main/java/net/dialingspoon/partialhearts/fabric/mixin/GuiMixin.java
@@ -1,5 +1,6 @@
 package net.dialingspoon.partialhearts.fabric.mixin;
 
+import net.dialingspoon.partialhearts.interfaces.IGui;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.entity.player.Player;
@@ -7,7 +8,6 @@ import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,11 +15,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Gui.class)
 public abstract class GuiMixin {
     @Shadow @Nullable protected abstract Player getCameraPlayer();
-    @Unique
-    public float displayHealthFloat;
 
     @Inject(method = "renderPlayerHealth", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/Gui;displayHealth:I", opcode = Opcodes.PUTFIELD))
     private void floatDisplayHealthUpdate(GuiGraphics guiGraphics, CallbackInfo ci) {
-        displayHealthFloat = getCameraPlayer().getHealth();
+        ((IGui)this).setdisplayHealthFloat(getCameraPlayer().getHealth());
     }
 }
--- neoforge/src/main/java/net/dialingspoon/partialhearts/neoforge/mixin/GuiMixin.java
+++ neoforge/src/main/java/net/dialingspoon/partialhearts/neoforge/mixin/GuiMixin.java
@@ -1,5 +1,6 @@
 package net.dialingspoon.partialhearts.neoforge.mixin;
 
+import net.dialingspoon.partialhearts.interfaces.IGui;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.entity.player.Player;
@@ -15,11 +16,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Gui.class)
 public abstract class GuiMixin {
     @Shadow @Nullable protected abstract Player getCameraPlayer();
-    @Unique
-    public float displayHealthFloat;
 
     @Inject(method = "renderHealthLevel(Lnet/minecraft/client/gui/GuiGraphics;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/Gui;displayHealth:I", opcode = Opcodes.PUTFIELD))
     private void floatDisplayHealthUpdate(GuiGraphics guiGraphics, CallbackInfo ci) {
-        displayHealthFloat = getCameraPlayer().getHealth();
+        ((IGui)this).setdisplayHealthFloat(getCameraPlayer().getHealth());
     }
 }
